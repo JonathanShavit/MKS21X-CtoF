bf7ab507d151eb55f40d5d6457125673740c3104
Jonathan Shavit <jshavit30@stuy.edu>
2021-09-24 08:05:00 -0400

    wrote the methods and test statements n main

--- CtoFTester.java
+++ CtoFTester.java
@@ -2,8 +2,18 @@
 
 public class CtoFTester{
   public static void main(String[] args){
+    System.out.println(CtoF(-40.0));
+    System.out.println(CtoF(37.0));
     System.out.println();
+    System.out.println(FtoC(-40.0));
+    System.out.println(FtoC(98.6));
   }
 
+  public static double CtoF(double c){
+    return c * 9.0 / 5.0 + 32;
+  }
 
+  public static double FtoC(double f){
+    return (f - 32) * 5.0 / 9.0;
+  }
 }
