11fdba5f5bafcec006b1f37af44bebc63a2c3074
Jonathan Shavit <jshavit30@stuy.edu>
2021-09-24 07:52:53 -0400

    empty file added

---
+++ CtoFTester.java
@@ -0,0 +1,9 @@
+// both should have a double(the temperature) as a parameter. they should also both be type double, since they need to return another double(also temperature)
+
+public class CtoFTester{
+  public static void main(String[] args){
+    System.out.println();
+  }
+
+
+}
